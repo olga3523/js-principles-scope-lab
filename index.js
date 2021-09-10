de9c6e0dc7d5ea03c7a26a287554eb639f00e223
OLGA <olichka23.85@gmail.com>
2021-09-10 11:22:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+    window.customerName = "BOB";
+    return window.customerName;
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    throw "Assignment to constant variable.";
+}
